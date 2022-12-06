01bdd75b8849a136c76d401b30dfb90b4dbf1199
Falifah <103418875+Falifah@users.noreply.github.com>
2022-12-06 10:03:04 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    define('DB_NAME', 'elearningbinaniaga');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_HOST', 'localhost');
+
+    $kon =mysqli_connect("localhost", "root", "", "elearningbinaniaga") or die(mysqli_error());
+?>
